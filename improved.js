5bb811050d76dae0eb632ed1dce44496f0e71338
SmajdaBence <smajdaben@gmail.com>
2026-04-16 17:52:14 +0200

    Add files via upload
    fejlesztett verzió minimálisan több funkcióval és szubjektívan jobb GUI-val

---
+++ improved.js
@@ -0,0 +1,18 @@
+const input = document.querySelector('#fasz')
+const suli = document.querySelector("#suli")
+
+input.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault(); // ne reloadoljon a form
+
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+
+    span.textContent = input.value;
+    li.appendChild(span);
+
+    suli.appendChild(li);
+
+    input.value = ""; // input törlése
+  }
+});
